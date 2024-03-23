e1aaf5f2dc0997e3cf61c05e8371a7f06e760e3c
Sayali <thengalsayali05@gmail.com>
2024-03-23 20:55:52 +0530

    count down

---
+++ Countdown_Timer/js/app.js
@@ -0,0 +1,31 @@
+const endDate = "24 March 2024 08:55 PM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date()
+    const diff = (end - now)/ 1000;
+
+
+    if (diff < 0) return;
+    // convert into days
+    inputs[0].value = Math.floor(diff/ 3600/ 24);
+    // convert into hours
+    inputs[1].value = Math.floor(diff/ 3600) % 24;
+    // convert into mins
+    inputs[2].value = Math.floor(diff/ 60) % 60;
+    // convert into seconds
+    inputs[3].value = Math.floor(diff) % 60;
+
+
+}
+
+clock();
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
